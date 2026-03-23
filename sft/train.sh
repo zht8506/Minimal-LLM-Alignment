f2e16c696e82efb6127993b7922c935c5d0fdfeb
zht8506 <81303574+zht8506@users.noreply.github.com>
2026-03-23 15:35:48 +0800

    Update train.sh
    Added support for multi-GPU training in train.sh.

--- sft/train.sh
+++ sft/train.sh
@@ -8,6 +8,7 @@ dataset_path=$SCRIPT_DIR/data/sft_en_demo.json
 
 output_dir=$SCRIPT_DIR/output_dir
 
+# single gpu training
 python "$SCRIPT_DIR/sft_train_ngpu.py" \
   --model_path $model_path \
   --train_json $dataset_path \
@@ -16,5 +17,17 @@ python "$SCRIPT_DIR/sft_train_ngpu.py" \
   --per_device_train_batch_size 8 \
   --gradient_accumulation_steps 16 \
   --learning_rate 2e-5 \
+  --max_length 4096 \
+  --bf16
+
+# multi-gpu training
+torchrun --nproc_per_node=2 "$SCRIPT_DIR/sft_train_ngpu.py" \
+  --model_path $model_path \
+  --train_json $dataset_path \
+  --output_dir $output_dir \
+  --per_device_train_batch_size 8 \
+  --gradient_accumulation_steps 8 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 10 \
   --max_length 4096 \
   --bf16
