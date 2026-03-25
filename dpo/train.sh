18f7ca0202f1a3d80f6e7e4db56a65464efc9cad
zht8506 <81303574+zht8506@users.noreply.github.com>
2026-03-25 20:20:25 +0800

    Create train.sh

---
+++ dpo/train.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+model_path=/root/your_model_path/
+
+dataset_path=$SCRIPT_DIR/data/dpo_en_demo.json
+
+output_dir=$SCRIPT_DIR/output_dir
+
+# single gpu training
+python "$SCRIPT_DIR/dpo_train.py" \
+  --model_path $model_path \
+  --train_json $dataset_path \
+  --output_dir $output_dir \
+  --num_train_epochs 2 \
+  --per_device_train_batch_size 8 \
+  --gradient_accumulation_steps 2 \
+  --learning_rate 2e-5 \
+  --max_length 4096 \
+  --bf16
+
+# multi-gpu training
+torchrun --nproc_per_node=2 "$SCRIPT_DIR/dpo_train_ngpu.py" \
+  --model_path $model_path \
+  --train_json $dataset_path \
+  --output_dir $output_dir \
+  --per_device_train_batch_size 8 \
+  --gradient_accumulation_steps 2 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 10 \
+  --max_length 4096 \
+  --bf16
