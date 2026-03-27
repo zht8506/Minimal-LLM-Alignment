ca74e83735f944a9781eeb5c356986d00e47fa80
zht8506 <81303574+zht8506@users.noreply.github.com>
2026-03-27 23:41:41 +0800

    Update simpo_train.py

--- simpo/simpo_train.py
+++ simpo/simpo_train.py
@@ -14,10 +14,12 @@ from contextlib import nullcontext
 from torch.utils.data import DataLoader
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-DATA_PATH = Path(__file__).parent.parent / "datasets"
-sys.path.insert(0, str(DATA_PATH))
+# Ensure local dataset module can be imported no matter where this script is launched.
+CURRENT_DIR = Path(__file__).resolve().parent
+if str(CURRENT_DIR) not in sys.path:
+    sys.path.insert(0, str(CURRENT_DIR))
 
-from dpo_dataset import DPODataCollator, JsonDPODataset
+from dpo_dataset import JsonDPODataset, DPODataCollator
 
 
 def _get_batch_logps(
@@ -25,6 +27,7 @@ def _get_batch_logps(
 ) -> torch.FloatTensor:
     """
     Calculate the GT label log probabilities of each sample.
+    average_log_prob = True for simpo.
     """
     assert logits.shape[:-1] == labels.shape
     labels = labels[:, 1:].clone()
@@ -36,22 +39,19 @@ def _get_batch_logps(
         return (per_token_logps * loss_mask).sum(-1) / loss_mask.sum(-1)
     return (per_token_logps * loss_mask).sum(-1)
 
-def preference_loss(
+def simpo_loss(
     policy_chosen_logps: torch.FloatTensor,
     policy_rejected_logps: torch.FloatTensor,
-    reference_chosen_logps: torch.FloatTensor,
-    reference_rejected_logps: torch.FloatTensor,
     beta: float,
+    gamma: float, # reward margin
 ) -> Tuple[torch.FloatTensor, torch.FloatTensor, torch.FloatTensor]:
     
     pi_logratios = policy_chosen_logps - policy_rejected_logps
-    ref_logratios = reference_chosen_logps - reference_rejected_logps
-    logits = pi_logratios - ref_logratios
 
-    losses = -F.logsigmoid(beta * logits)
+    losses = -F.logsigmoid(beta * pi_logratios - gamma)
 
-    chosen_rewards = beta * (policy_chosen_logps - reference_chosen_logps).detach()
-    rejected_rewards = beta * (policy_rejected_logps - reference_rejected_logps).detach()
+    chosen_rewards = beta * policy_chosen_logps.detach()
+    rejected_rewards = beta * policy_rejected_logps.detach()
 
     return losses, chosen_rewards, rejected_rewards
 
@@ -163,24 +163,16 @@ class BaseTrainer:
         self._save_checkpoint("final")
 
 
-class DPOTrainer(BaseTrainer):
-    def __init__(self, policy, ref_model, tokenizer, train_loader: DataLoader, args):
-        super().__init__(policy, tokenizer, train_loader, args)
-        self.ref_model = ref_model
-        self.ref_model.to(self.device)
-        self.ref_model.eval()
-        for p in self.ref_model.parameters():
-            p.requires_grad = False
+class SimPOTrainer(BaseTrainer):
 
     def separate_forward(self, model: torch.nn.Module, batch: Dict) -> Tuple[torch.Tensor, torch.Tensor]:
-
         # Forward for chosen
         chosen_logits = model(
             batch["chosen_input_ids"],
             attention_mask=batch["chosen_attention_mask"],
         ).logits.to(torch.float32)
         chosen_logps = _get_batch_logps(
-            chosen_logits, batch["chosen_labels"], average_log_prob=False
+            chosen_logits, batch["chosen_labels"], average_log_prob=True
         )
 
         # Forward for rejected
@@ -189,22 +181,19 @@ class DPOTrainer(BaseTrainer):
             attention_mask=batch["rejected_attention_mask"],
         ).logits.to(torch.float32)
         rejected_logps = _get_batch_logps(
-            rejected_logits, batch["rejected_labels"], average_log_prob=False
+            rejected_logits, batch["rejected_labels"], average_log_prob=True
         )
 
         return chosen_logps, rejected_logps
 
     def get_batch_metrics(self, batch, train: bool = True):
         policy_chosen_logps, policy_rejected_logps = self.separate_forward(self.model, batch)
-        with torch.no_grad():
-            reference_chosen_logps, reference_rejected_logps = self.separate_forward(self.ref_model, batch)
 
-        losses, chosen_rewards, rejected_rewards = preference_loss(
+        losses, chosen_rewards, rejected_rewards = simpo_loss(
             policy_chosen_logps,
             policy_rejected_logps,
-            reference_chosen_logps,
-            reference_rejected_logps,
-            beta=self.args.beta
+            beta=self.args.beta,
+            gamma=self.args.gamma
         )
 
         reward_acc = (chosen_rewards > rejected_rewards).float().mean().item()
@@ -227,7 +216,8 @@ def parse_args():
     parser.add_argument("--num_train_epochs", type=int, default=1)
     parser.add_argument("--per_device_train_batch_size", type=int, default=1)
     parser.add_argument("--gradient_accumulation_steps", type=int, default=8)
-    parser.add_argument("--beta", type=float, default=0.1)
+    parser.add_argument("--beta", type=float, default=2.0)
+    parser.add_argument("--gamma", type=float, default=1)
     # optimizer
     parser.add_argument("--learning_rate", type=float, default=2e-5)
     parser.add_argument("--weight_decay", type=float, default=0.0)
@@ -244,7 +234,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    print("Loading policy, reference, and tokenizer...")
+    print("Loading policy, and tokenizer...")
     policy = AutoModelForCausalLM.from_pretrained(
         args.model_path,
         trust_remote_code=True,
@@ -273,9 +263,8 @@ def main():
         collate_fn=data_collator,
     )
 
-    trainer = DPOTrainer(
-        policy=policy,
-        ref_model=ref_model,
+    trainer = SimPOTrainer(
+        model=policy,
         tokenizer=tokenizer,
         train_loader=train_loader,
         args=args,
